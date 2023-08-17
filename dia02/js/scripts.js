8112b7d72e1b8afa7aa4d9f1ba27689be665c76a
João Martins <82725748+Hidekidesu@users.noreply.github.com>
2023-08-17 11:04:02 -0300

    Initial commit

--- dia02/js/scripts.js
+++ dia02/js/scripts.js
@@ -1,7 +1,7 @@
 
-let nome = prompt("Qual o seu nome?");
-let idade = prompt("Quantos anos você tem?");
-let linguagem = prompt("Qual linguagem de programação você está estudando?");
+const nome = prompt("Qual o seu nome?");
+const idade = prompt("Quantos anos você tem?");
+const linguagem = prompt("Qual linguagem de programação você está estudando?");
 
 alert(`Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!`);
 
